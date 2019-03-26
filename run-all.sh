5e5a22a71e5b88ebcf2cf52e4d9222c930f1a6a8
Mark D. Blackwell <markdblackwell01@gmail.com>
2019-03-26 18:15:19 -0400

    Create shell script to run all the programs

---
+++ run-all.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Copyright (C) 2018 Mark D. Blackwell.
+#   All rights reserved.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ruby 02_HelloWorld_TheObligatoryFirstProgram_InstallingTk.rb &
+ruby 03_FeetToMeters_Code_AFirstRealExample.rb &
+ruby 04_EventBindings_EventHandling_TkConcepts.rb &
+ruby 06_1_SpanningMultipleCells_TheGridGeometryManager.rb &
+ruby 06_2_Padding_TheGridGeometryManager.rb &
+ruby 07_1_GiftsToCountries_Listbox_MoreWidgets.rb &
+ruby 07_2_OneHundredLines_Scrollbar_MoreWidgets.rb &
+ruby 08_1_Menubars_Menus.rb &
+ruby 08_2_ContextMenus_Menus.rb &
+ruby 09_1_StackingOrder_ChangingWindowBehaviorAndStyles_WindowsAndDialogs.rb &
+ruby 09_2_AlertAndConfirmationDialogs_StandardDialogs_WindowsAndDialogs.rb &
+ruby 10_1_PanedWindows_OrganizingComplexInterfaces.rb &
+ruby 10_2_Notebook_OrganizingComplexInterfaces.rb &
+ruby 12_1_Sketchpad_CreatingItems_Canvas.rb &
+ruby 12_2_ColorPicker_Bindings_Canvas.rb &
+ruby 12_3_Decoration_Tags_Canvas.rb &
+ruby 12_4_Visible_Scrolling_Canvas.rb &
+ruby 13_LoggingWindow_ModifyingTheTextInCode_Text.rb &
+ruby other/error_unknown_option.rb &
+ruby other/keypress_capture.rb &
+ruby other/name_unicode.rb &
