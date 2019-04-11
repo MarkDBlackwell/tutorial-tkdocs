937f258e236161570ce72611ec8ec5f3443d136a
Mark D. Blackwell <markdblackwell01@gmail.com>
2019-04-11 15:37:32 -0400

    Simplify

--- stackoverflow/how-can-i-determine-most-easily-the-keysyms-of-unusual-keys-on-my-keyboard.rb
+++ stackoverflow/how-can-i-determine-most-easily-the-keysyms-of-unusual-keys-on-my-keyboard.rb
@@ -4,47 +4,14 @@
 #  http://stackoverflow.com/questions/55602789/how-can-i-determine-most-easily-the-keysyms-of-unusual-keys-on-my-keyboard-in-r
 
 require 'tk'
-require 'tkextlib/tile'
 
 module ::KeypressCapture
   module GraphicalHelper
 
-    def f_content
-      $f_content_value ||= begin
-        f = ::Tk::Tile::Frame.new root
-        f.grid sticky: :wnes
-      end
-    end
-
-    def platform
-      @platform_value ||= ::Tk.windowingsystem
-    end
-
     def root
-      $root_value ||= begin
-        tell_tk_which_encoding_to_use
+      $root ||= begin
+        ::Tk::Encoding.encoding = ''.encoding
         ::TkRoot.new
-      end
-    end
-
-    private
-
-    def tell_tk_which_encoding_to_use
-      ::Tk::Encoding.encoding = ''.encoding
-      nil
-    end
-  end
-end
-
-module ::KeypressCapture
-  module GraphicalObjects
-
-    def f_frame_inner
-      @f_frame_inner_value ||= begin
-        f = ::Tk::Tile::Frame.new f_content
-        f.height 100
-        f.width 200
-        f.grid
       end
     end
   end
@@ -53,26 +20,18 @@ end
 module ::KeypressCapture
   module Graphical
     extend GraphicalHelper
-    extend GraphicalObjects
     extend self
 
     def main
-      f_content.padding '3 3 3 3'
-      f_frame_inner
-      events_bind
+      root.bind :KeyPress, lambda_keypress, '%k %K'
       ::Tk.mainloop
       nil
     end
 
     private
 
-    def events_bind
-      root.bind :KeyPress, lambda_keypress, '%k %K'
-      nil
-    end
-
     def lambda_keypress
-      @lambda_keypress_value ||= ::Kernel.lambda do |key_code, key_sym|
+      @lambda_keypress ||= ::Kernel.lambda do |key_code, key_sym|
         print "lambda_keypress invoked with keycode #{key_code} and keysym #{key_sym}.\n"
         nil
       end
