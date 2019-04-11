46b7cc88d207aaab9c1584b404d7daf0cc8f7097
Mark D. Blackwell <markdblackwell01@gmail.com>
2019-04-11 15:42:35 -0400

    Simplify

--- stackoverflow/how-can-i-determine-most-easily-the-keysyms-of-unusual-keys-on-my-keyboard.rb
+++ stackoverflow/how-can-i-determine-most-easily-the-keysyms-of-unusual-keys-on-my-keyboard.rb
@@ -6,37 +6,25 @@
 require 'tk'
 
 module ::KeypressCapture
-  module GraphicalHelper
+  extend self
 
-    def root
-      $root ||= begin
-        ::Tk::Encoding.encoding = ''.encoding
-        ::TkRoot.new
-      end
+  def lambda_keypress
+    @lambda_keypress ||= Kernel.lambda do |key_code, key_sym|
+      puts "lambda_keypress invoked with keycode #{key_code} and keysym #{key_sym}."
+    end
+  end
+
+  def main
+    root.bind :KeyPress, lambda_keypress, '%k %K'
+    Tk.mainloop
+  end
+
+  def root
+    $root ||= begin
+      Tk::Encoding.encoding = ''.encoding
+      TkRoot.new
     end
   end
 end
 
-module ::KeypressCapture
-  module Graphical
-    extend GraphicalHelper
-    extend self
-
-    def main
-      root.bind :KeyPress, lambda_keypress, '%k %K'
-      ::Tk.mainloop
-      nil
-    end
-
-    private
-
-    def lambda_keypress
-      @lambda_keypress ||= ::Kernel.lambda do |key_code, key_sym|
-        print "lambda_keypress invoked with keycode #{key_code} and keysym #{key_sym}.\n"
-        nil
-      end
-    end
-  end
-end
-
-::KeypressCapture::Graphical.main
+::KeypressCapture.main
